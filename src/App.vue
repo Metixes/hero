4cc4d35441e13618aa92be276f3a5ddbce6d0bbd
Metixes <metixes@gmail.com>
2024-01-24 18:54:33 +0200

    add: project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import "./assets/fonts/fornts.scss";
+
+:root {
+  --pink: #e37c8c;
+  --blue: #8898df;
+  --gr-grey: #eceef0;
+  --gr-white: #f9f9fa;
+}
+* {
+  font-family: "DFHei";
+  font-size: 12px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
